57f6090ad127a521c7ba4b8b8c4fda2f88bd07dc
yenvu93 <yenvu1794@gmail.com>
2023-04-12 12:53:55 +0100

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -76,6 +76,39 @@ void timer(int value)
 	glutTimerFunc(model.FRAME_TIME, timer, value);
 }
 
+/*
+int main(int argc, char** argv)
+{
+	if (argc < 3) {
+		printf("Usage: %s hotname port\n", argv[0]);
+		return 0;
+	}
+	if (Application::client_->start(argv[1], argv[2])) {
+		Application::client_->interact();
+		Application::client_->sendthis("Hi");
+		std::cout << "Receiving the number of players: " << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::map< std::string, std::string> players = Application::client_->deserizalize();
+		
+		stringstream strValue;
+		strValue << players.size();
+		strValue >> Application::playerNumber;
+
+		std::cout << "Number of Player: " << Application::playerNumber << std::endl;
+	}
+	static std::shared_ptr<Application> application = std::make_shared< Application>(&argc, argv);
+	std::shared_ptr<View> view = application->getView();
+	std::shared_ptr<Model> model = application->getModel();
+	model->initScreen(Application::playerNumber);
+	view->texturize(model, Application::playerNumber);
+	application->start();
+
+	Application::client_->terminate();
+	WSACleanup();
+
+	return 0;
+}
+*/
 /**
  * Main function.
  */
